e1e9ceb14347420a8679f85c47c0dccc45351aec
Drewplonq <it@goplonq.com>
2024-09-29 19:19:05 +0300

    Update vms_platform.tf

--- vms_platform.tf
+++ vms_platform.tf
@@ -48,11 +48,12 @@ variable "vm_db_vpc_name" {
 }
 ###ssh vars
 
-variable "vms_ssh_root_key" {
+/* variable "vms_ssh_root_key" {
   type        = string
   default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
 }
+*/
 variable "vm_web_fam" {
   type        = string
   default     = "ubuntu-2004-lts"
@@ -63,7 +64,7 @@ variable "vm_db_fam" {
   default     = "ubuntu-2004-lts"
   description = "yandex_compute_image"
 }
-variable "vm_web_name" {
+ variable "vm_web_name" {
   type        = string
   default     = "netology-develop-platform-web"
   description = "yandex_compute_instance"
@@ -73,7 +74,7 @@ variable "vm_db_name" {
   default     = "netology-develop-platform-db"
   description = "yandex_compute_instance"
 }
-variable "vm_web_platform" {
+/* variable "vm_web_platform" {
   type        = tuple([ string, number, number, number, bool, bool ])
   default     = ["standard-v1", 2, 1, 5, true, true]
   description = "yandex_compute_image"
@@ -82,4 +83,39 @@ variable "vm_db_platform" {
   type        = tuple([ string, number, number, number, bool, bool ])
   default     = ["standard-v3", 2, 2, 20, true, true]
   description = "yandex_compute_image"
+}
+*/
+variable "vms_resources" {
+    type    = map(object({
+        platform_id = string
+        cores   =   number
+        memory  =   number
+        core_fraction   =   number
+        preemptible = bool
+        nat = bool
+    }))
+    default = {
+      "web" = {
+        platform_id = "standard-v1"
+        cores   =  2
+        memory  =  1
+        core_fraction   =   5
+        preemptible = true
+        nat = true
+      }
+      "db" = {
+        platform_id = "standard-v3"
+        cores   =  2
+        memory  =  2
+        core_fraction   =   20
+        preemptible = true
+        nat = true
+      }
+    }
+    description = "vms resources"
+}
+
+variable "vms_metadata" {
+  type = map(string)
+    description = "Metadata of Vm's"
 }
