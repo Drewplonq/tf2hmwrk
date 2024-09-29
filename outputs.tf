c9bd90fab3e9a80ca5cd667343a4ae1df54403bd
Drewplonq <it@goplonq.com>
2024-09-29 17:39:42 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "VMs" {
+output "about_vm" {
     value = [
         [yandex_compute_instance.platform.name, yandex_compute_instance.platform.network_interface.0.nat_ip_address, yandex_compute_instance.platform.fqdn],
         [yandex_compute_instance.db.name, yandex_compute_instance.db.network_interface.0.nat_ip_address, yandex_compute_instance.db.fqdn]
