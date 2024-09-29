ec08d194186f0295da07c5e8ddfdf4a6682ad546
Drewplonq <it@goplonq.com>
2024-09-29 17:39:21 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "VMs" {
     value = [
         [yandex_compute_instance.platform.name, yandex_compute_instance.platform.network_interface.0.nat_ip_address, yandex_compute_instance.platform.fqdn],
-        [yandex_compute_instance.db.name, yandex_compute_instance.db.network_interface.0.nat_ip_address, yandex_compute_instance.platform_db.fqdn]
+        [yandex_compute_instance.db.name, yandex_compute_instance.db.network_interface.0.nat_ip_address, yandex_compute_instance.db.fqdn]
         ]
 }
